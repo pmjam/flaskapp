05532b33e36f70a2c7c485e2b169db5703788053
pmjam <>
2020-07-17 20:09:31 -0300

    added queryset manager

--- flaskapp/models/base.py
+++ flaskapp/models/base.py
@@ -7,6 +7,11 @@ class BaseDocument(me.DynamicDocument):
     created_at = me.DateTimeField(default=datetime.datetime.now)
     updated_at = me.DateTimeField()
     is_active = me.BooleanField(default=True)
+
+    @me.queryset.queryset_manager
+    def objects(doc_cls, queryset):
+        return queryset.filter(is_active=True)
+
     meta = {
         'abstract': True,
     }

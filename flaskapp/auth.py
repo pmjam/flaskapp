33b711487d2d7f8d0dbfd5ec0298f26315902e7e
pmjam <>
2020-07-19 18:23:27 -0300

    user logged

--- flaskapp/auth.py
+++ flaskapp/auth.py
@@ -5,8 +5,9 @@ from flask_login import LoginManager
 
 
 bcrypt = Bcrypt()
-login_manager = LoginManager()
 service = ServiceUser()
+login_manager = LoginManager()
+login_manager.login_view = "login.signin"
 
 
 def init_app(app):

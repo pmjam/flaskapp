9cde16b5516edb1233dd1630a16f69037be139bc
pmjam <>
2020-07-20 16:05:13 -0300

    flask-cors

--- flaskapp/auth.py
+++ flaskapp/auth.py
@@ -3,8 +3,10 @@ from flaskapp.services import ServiceUser
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 
+cors = CORS()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 service = ServiceUser()
@@ -13,6 +15,7 @@ login_manager.login_view = 'login.signin'
 
 
 def init_app(app):
+    cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
     bcrypt.init_app(app)
     jwt.init_app(app)
     login_manager.init_app(app)
